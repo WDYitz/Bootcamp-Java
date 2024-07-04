234de8f58608a9bd9ca686de5da8fcd6d0e7940d
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-07-04 15:46:12 -0300

    feat: add tags study

---
+++ src/edu/yitzhak/exercice/Calculator.java
@@ -0,0 +1,26 @@
+package edu.yitzhak.exercice;
+
+/**
+ * <h3>Calculator</h3>
+ * The Calculator class implements an application that simply performs basic
+ * arithmetic operations.
+ * 
+ * <b>Note:</b> This class is part of the edu.yitzhak.exercice package.
+ * 
+ * @author Yitzhak Rodriguez
+ * @version 1.0
+ * @since 2024-07-01
+ */
+
+public class Calculator {
+  /**
+   * This method adds two numbers.
+   * @param firstNumber
+   * @param secondNumber
+   * @return int This returns the sum of two numbers.
+   */
+
+  public int add(int firstNumber, int secondNumber) {
+    return firstNumber + secondNumber;
+  }
+}
