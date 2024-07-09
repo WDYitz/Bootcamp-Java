307b803928cadae5e0c07f3ab16cd8b113610834
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-07-09 13:10:43 -0300

    feat: add syntax study using csharp, python, kotlin and javascript

---
+++ src/com/yitzhak/exercice/SyntaxExercice/SalaryCalculator.java
@@ -0,0 +1,29 @@
+package com.yitzhak.exercice.SyntaxExercice;
+
+import static java.lang.StringTemplate.STR;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Calculo de salario");
+
+    System.out.println("Digite o valor do salario: ");
+    float valorSalario = scanner.nextFloat();
+    System.out.println("Digite o valor dos beneficios: ");
+    float valorBeneficios = scanner.nextFloat();
+
+    float valorImposto = 0;
+    if (valorSalario >= 0 && valorSalario <= 1100) {
+      valorImposto = 0.05F * valorSalario;
+    } else if (valorSalario > 1100 && valorSalario <= 2500) {
+      valorImposto = 0.10F * valorSalario;
+    } else if (valorSalario > 2500) {
+      valorImposto = 0.15F * valorSalario;
+    }
+
+    float saida = valorSalario - valorImposto + valorBeneficios;
+    System.out.println(STR."Salario liquido: \{saida}");
+  }
+}
--- src/com/yitzhak/firstweek/exceptions/Exceptions.java
+++ src/com/yitzhak/firstweek/exceptions/Exceptions.java
@@ -6,16 +6,10 @@ import java.util.Scanner;
 
 public class Exceptions {
   public static void main(String[] args) {
-    System.out.println("Hello, I'm Yitzhak!");
-
     try {
       Scanner scanner = new Scanner(System.in);
 
-      System.out.println("Digite seu nome: ");
-      String name = scanner.next();
-
-      System.out.println("Digite sua idade: ");
-      int age = scanner.nextInt();
+      System.out.println("Calculo de IMC");
 
       System.out.println("Digite seu peso: ");
       double weight = scanner.nextDouble();
@@ -23,6 +17,7 @@ public class Exceptions {
       System.out.println("Digite sua altura: ");
       double height = scanner.nextDouble();
 
+      System.out.println(STR."Seu IMC: \{(weight / (height * height))}");
 
     } catch (Exception e) {
      System.out.println(STR."Erro ao ler os dados. Tente novamente. \{e.getMessage()}");
