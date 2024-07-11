54cd9ce0347a6d6a69f40cfc78c0904bcc53c5e5
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-07-11 14:06:28 -0300

    feat: add more examples of dependency injection

--- src/com/yitzhak/firstweek/Poo/Poo.java
+++ src/com/yitzhak/firstweek/Poo/Poo.java
@@ -8,11 +8,14 @@ import com.yitzhak.firstweek.Poo.Class.Student;
 @SuppressWarnings("unused")
 public class Poo {
   public static void main(String[] args) {
-    Student student = new Student("Yitzhak", 22, "Male");
-    School school = new School("Miami Coral Park", 2004, new Object[] { student });
+    Student student1 = new Student("Yitzhak", 17, "Male");
+    Student student2 = new Student("Lucas", 16, "Male");
+    Student student3 = new Student("Camila", 16, "Female");
+    School school = new School("Miami Coral Park", 2004, new Object[] { student1, student2, student3  });
 
     String studentName = school.getStudentName("Yitzhak");
 
-    System.out.println(STR."\{studentName} is a student of \{school.getSchoolName()} that was founded in \{school.getYearOfFoundation()}.");
+    System.out.println(STR."\{studentName} is a student of \{school.getSchoolName()} that was founded in \{school.getYearOfFoundation()}, The school has \{school.getStudents().length} students.");
+    
   }
 }
